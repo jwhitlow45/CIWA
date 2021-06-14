f940ab4136eb9026181a23fe742caf5a6dbe8310
jwhitlow45 <jwhitlow45@gmail.com>
2021-06-14 18:51:20 -0400

    feat:add daily raw schemas

--- cimis/shared/raw_data/schemas.py
+++ cimis/shared/raw_data/schemas.py
@@ -3,11 +3,12 @@ from typing import Any, List, Optional
 
 import pydantic
 
+# Hourly Raw Data Schemas
 class HourlyRawBase(pydantic.BaseModel):
     
     Date: datetime.date
     Hour: datetime.time
-    HlyAirTemp = Optional[dict]
+    HlyAirTmp = Optional[dict]
     HlyDewPnt = Optional[dict]
     HlyEto = Optional[dict]
     HlyNetRad = Optional[dict]
@@ -31,7 +32,7 @@ class HourlyRawBase(pydantic.BaseModel):
                 isinstance(other, HourlyRawBase),
                 self.Date == other.Date ,
                 self.Hour == other.Hour,
-                self.HlyAirTemp == other.HlyAirTemp,
+                self.HlyAirTmp == other.HlyAirTmp,
                 self.HlyDewPnt == other.HlyDewPnt,
                 self.HlyEto == other.HlyEto,
                 self.HlyNetRad == other.HlyNetRad,
@@ -82,3 +83,79 @@ class HourlyRaw(HourlyRawBase):
             ]
         )
 
+# Daily Raw Data Schemas
+class DailyRawBase(pydantic.BaseModel):
+
+    Date: datetime.date
+    DayAirTmpAvg: Optional[dict]
+    DayAirTmpMax: Optional[dict]
+    DayAirTmpMin: Optional[dict]
+    DayDewPnt: Optional[dict]
+    DayAsceEto: Optional[dict]
+    DayPrecip: Optional[dict]
+    DayRelHumAvg: Optional[dict]
+    DayRelHumMax: Optional[dict]
+    DayRelHumMin: Optional[dict]
+    DaySoilTmpAvg: Optional[dict]
+    DaySolRadAvg: Optional[dict]
+    DayVapPresAvg: Optional[dict]
+    DayWindRun: Optional[dict]
+    DayWindSpdAvg: Optional[dict]
+
+    class Config:
+        extra = 'ignore'
+
+    def __eq__(self, other: Any) -> bool:
+        return all(
+            [
+                isinstance(other, DailyRawBase),
+                self.Date == other.Date,
+                self.DayAirTmpAvg == other.DayAirTmpAvg,
+                self.DayAirTmpMax == other.DayAirTmpMax,
+                self.DayAirTmpMin == other.DayAirTmpMin,
+                self.DayDewPnt == other.DayDewPnt,
+                self.DayAsceEto == other.DayAsceEto,
+                self.DayPrecip == other.DayPrecip,
+                self.DayRelHumAvg == other.DayRelHumAvg,
+                self.DayRelHumMax == other.DayRelHumMax,
+                self.DayRelHumMin == other.DayRelHumMin,
+                self.DaySoilTmpAvg == other.DaySoilTmpAvg,
+                self.DaySolRadAvg == other.DaySolRadAvg,
+                self.DayVapPresAvg == other.DayVapPresAvg,
+                self.DayWindRun == other.DayWindRun,
+                self.DayWindSpdAvg == other.DayWindSpdAvg
+            ]
+        )
+
+class DailyRawInCimisResponse(DailyRawBase):
+
+    Station: int
+
+    class Config:
+        extra = 'ignore'
+
+    def __eq__(self, other: Any) -> bool:
+        return all(
+            [
+                super().__eq__(other),
+                isinstance(other, DailyRawInCimisResponse),
+                self.Station == other.Station
+            ]
+        )
+
+class DailyRaw(DailyRawBase):
+
+    Id: int
+
+    class Config:
+        extra = 'ignore'
+        orm_mode = True
+
+    def __eq__(self, other: Any) -> bool:
+        return all(
+            [
+                super().__eq__(other),
+                isinstance(other, DailyRaw),
+                self.Id == other.Id
+            ]
+        )
