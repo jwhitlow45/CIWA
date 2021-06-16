0a398a30c0bff3ccd700bcd3aa559f6951ff34b5
jwhitlow45 <jwhitlow45@gmail.com>
2021-06-16 14:06:19 -0400

    db-migration:differentiate id from stationid

--- cimis/shared/raw_data/models.py
+++ cimis/shared/raw_data/models.py
@@ -6,6 +6,7 @@ class HourlyRawData(Base):
     __tablename__ = 'HourlyRaw'
 
     Id = Column(Integer, primary_key=True, nullable=False)
+    StationId = Column(Integer, nullable=False)
     Date = Column(Date, nullable=False)
     Hour = Column(Time, nullable=False)
     
@@ -70,6 +71,7 @@ class DailyRawData(Base):
     __tablename__ = 'DailyRaw'
 
     Id = Column(Integer, primary_key=True, nullable=False)
+    StationId = Column(Integer, nullable=False)
     Date = Column(Date, nullable=False)
     
     DayAirTmpAvg = Column(Float, nullable=True)
