a84507f4d8071f6800e1b34bbeca95ebb8f67880
jwhitlow45 <jwhitlow45@gmail.com>
2021-06-16 16:57:41 -0400

    bug:DayRelHumMin precision limit of 50 removed

--- cimis/shared/raw_data/models.py
+++ cimis/shared/raw_data/models.py
@@ -5,7 +5,7 @@ from shared.core.db import Base
 class HourlyRawData(Base):
     __tablename__ = 'HourlyRaw'
 
-    Id = Column(Integer, primary_key=True, nullable=False)
+    Id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     StationId = Column(Integer, nullable=False)
     Date = Column(Date, nullable=False)
     Hour = Column(Time, nullable=False)
@@ -70,7 +70,7 @@ class HourlyRawData(Base):
 class DailyRawData(Base):
     __tablename__ = 'DailyRaw'
 
-    Id = Column(Integer, primary_key=True, nullable=False)
+    Id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     StationId = Column(Integer, nullable=False)
     Date = Column(Date, nullable=False)
     
@@ -114,7 +114,7 @@ class DailyRawData(Base):
     DayRelHumMaxQc = Column(String(50), nullable=True, default='')
     DayRelHumMaxUnits = Column(String(50), nullable=True, default='')
     
-    DayRelHumMin = Column(Float(50), nullable=True)
+    DayRelHumMin = Column(Float, nullable=True)
     DayRelHumMinQc = Column(String(50), nullable=True, default='')
     DayRelHumMinUnits = Column(String(50), nullable=True, default='')
     
