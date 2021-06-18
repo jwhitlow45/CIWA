60c5d25e68f8bb452e061ad6ff5e67ca664fe72b
jwhitlow45 <jwhitlow45@gmail.com>
2021-06-18 15:20:42 -0400

    refactor:change name of ActionType to add daily raw

--- cimis/shared/message/actions.py
+++ cimis/shared/message/actions.py
@@ -8,8 +8,8 @@ from shared.message import payloads
 class ActionType(str, Enum):
     STATIONS_ADD = 'stations/add'
     STATIONS_UPDATE = 'stations/update'
-    DATA_GET_HOURLY_RAW = 'data/get_hourly_raw'
-    DATA_GET_DAILY_RAW = 'data/get_daily_raw'
+    DATA_ADD_HOURLY_RAW = 'data/add_hourly_raw'
+    DATA_ADD_DAILY_RAW = 'data/add_daily_raw'
 
 
 class Action(pydantic.BaseModel):
@@ -27,11 +27,11 @@ class UpdateStationsAction(Action):
     payload: payloads.UpdateStationsPayload
 
 
-class GetHourlyRawDataAction(Action):
-    action_type = ActionType.DATA_GET_HOURLY_RAW
+class AddHourlyRawDataAction(Action):
+    action_type = ActionType.DATA_ADD_HOURLY_RAW
     payload: payloads.GetHourlyRawPayload
 
 
-class GetDailyRawDataAction(Action):
-    action_type = ActionType.DATA_GET_DAILY_RAW
+class AddDailyRawDataAction(Action):
+    action_type = ActionType.DATA_ADD_DAILY_RAW
     payload: payloads.GetDailyRawPayload
