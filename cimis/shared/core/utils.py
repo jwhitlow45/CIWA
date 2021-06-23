88bfdc54c57c18cfaf06b7a3028df9724541d631
jdubbleu <jwhitlow45@gmail.com>
2021-06-23 13:19:50 -0400

    refactor:change functions to check if cimis record limit is exceeded to 1 function

--- cimis/shared/core/utils.py
+++ cimis/shared/core/utils.py
@@ -112,14 +112,9 @@ def generate_raw_data_primary_key(station_num: int, date: datetime.date, hour: d
     hour_key = int_to_binary_string(hour.hour, 5)
     return int(station_key + date_key + hour_key, 2)
 
-def is_below_cimis_hourly_record_limit(targets: List[int], start_date: datetime.date, end_date:datetime.date) -> bool:
-    """Check if hourly request is exceeding record limit set by CIMIS"""
+def is_below_cimis_record_limit(targets: List[int], start_date: datetime.date,
+                                        end_date: datetime.date, records_per_day: int) -> bool:
+    """Check if request is exceeding record limit set by CIMIS"""
     days_requested = (end_date - start_date).days + 1
-    # Return the result of (days requested * hours per day * number of stations) <= 1750
-    return (days_requested*24*len(targets)) <= 1750
-
-def is_below_cimis_daily_record_limit(targets: List[int], start_date: datetime.date, end_date:datetime.date):
-    """Check if daily request is exceeding record limit set by CIMIS"""
-    days_requested = (end_date - start_date).days + 1
-    # Return the result of (days requested * number of stations) <= 1750
-    return (days_requested*len(targets)) <= 1750
+    # Return the result of (days requested * records per day * number of stations) <= 1750
+    return (days_requested*records_per_day*len(targets)) <= 1750
